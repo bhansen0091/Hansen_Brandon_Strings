38d05270a501cdc1f36eaa4beb84dd2517846e33
bhansen0091 <bhansen0091@yahoo.com>
2021-04-09 13:52:44 -0500

    initial commit

---
+++ src/test/java/com/hansen/strings/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansen.strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
